d7202fa1659b672726dd23b3e095fce5b4690196
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-10-18 20:21:07 +0100

    Add state to searchbar to capture input from user

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/SearchBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className={styles.container}>
       <input
         className={styles.searchBar}
         placeholder="Search for your favoirte movie"
+        onChange={(event) => setSearchTerm(event.target.value)}
       ></input>
       <button className={styles.searchButton}>
         <FontAwesomeIcon
           className={styles.icon}
           icon={faSearch}
+          onClick={() => alert(searchTerm)}
         ></FontAwesomeIcon>
       </button>
     </div>
