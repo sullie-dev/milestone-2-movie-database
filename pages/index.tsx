a3df8ec9a703f0ddf0b919ec1a5bdbcc4ba5bbc3
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-10-18 19:30:48 +0100

    Import head into index.tsx and set basic structure for the head in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <div>
+      <Head>
+        <title>Milestone 2 | Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Milestone 2 project for Code Institute's Software development diploma course. The site will be a movie database site "
+        />
+        <meta
+          name="keywords"
+          content="movies, movie database, movies tv shows"
+        />
+      </Head>
+    </div>
+  );
 };
 
 export default Home;
